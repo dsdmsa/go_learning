254271dfaf7d51da42f98d2dbc538a201e152675
Urmanschi Mihail <urmanschi.mihail@gmail.com>
2022-01-26 22:24:38 -0800

    math trik game

--- lec2/main.go
+++ lec2/main.go
@@ -10,9 +10,9 @@ const PROMPT = "and press ENTER when ready."
 
 func main() {
 
-	var num1 = 1
+	var num1 = 2
 	var num2 = 5
-	var num3 = 4
+	var num3 = 7
 	var answer int
 
 	reader := bufio.NewReader(os.Stdin)
@@ -21,9 +21,22 @@ func main() {
 	fmt.Println("==================")
 	fmt.Println("")
 
-	fmt.Println(" inpiut a num, 1-10", PROMPT)
+	fmt.Println(" inpiut a num, 1 and 10", PROMPT)
 	reader.ReadString('\n')
 
 	fmt.Println(" multiply your number by", num1, PROMPT)
+	reader.ReadString('\n')
+
+	fmt.Println(" multiply your number by", num2, PROMPT)
+	reader.ReadString('\n')
+
+	fmt.Println(" divide the result by your original number", PROMPT)
+	reader.ReadString('\n')
+
+	fmt.Println(" now substract", num3, PROMPT)
+	reader.ReadString('\n')
+
+	answer = num1*num2 - num3
+	fmt.Println("the answer is", answer)
 
 }
